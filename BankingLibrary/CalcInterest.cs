a996b7a904eef6ba44f8cb11ccfca69e5cd835ec
Julia <juliakurakula01@gmail.com>
2021-09-17 17:40:35 +0530

    first commit

---
+++ BankingLibrary/CalcInterest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingLibrary
+{
+   public class CalcInterest : ICalcInterest
+    {
+        public int Calculate (int principle,int years,int RateOfInt)
+        {
+            int SimpInt = (principle * years * RateOfInt) / 100;
+            return SimpInt;
+        }
+
+    }
+}
